1a38bed4f43190062bad5fc9520be25ccde2993e
nicopoko <84250795+nikopoko@users.noreply.github.com>
2024-07-16 10:40:13 +0300

    .

--- tontools1.py
+++ tontools1.py
@@ -1,11 +1,15 @@
 import asyncio
 import time
 from TonTools import Wallet, LsClient, TonApiClient
-from ton import TonlibClient
-#from ton.sync import TonlibClient
+from pytonapi import Tonapi
+
+API_KEY = "AFH7AUTQMY4CKMAAAAAA76X6K36GZN2YICGMSAS2Q5TOIL3WRXCKQ7QCFORMLXAIXUQWICA"
 
 
-SUMMA = 5 #сколько тонов должно прийти
+ACCOUNT_ID = "kQBWtsaGAc7WQInphnDBaz6D0LqVW09ysY1rNX5RcKnI_ElC" 
+
+
+SUMMA = 6 #сколько тонов должно прийти
 
 loop = asyncio.get_event_loop()
 
@@ -13,7 +17,7 @@ end_adress = "ВАШ АДРЕС КОШЕЛЬКА" #ВПИШИТЕ СВОЙ КО�
     
 
 async def trans_to_wallet(end_adress, SUMMA):
-    provider = TonApiClient('AFH7AUTQMY4CKMAAAAAA76X6K36GZN2YICGMSAS2Q5TOIL3WRXCKQ7QCFORMLXAIXUQWICA',addresses_form='user_friendly',testnet=True)
+    provider = TonApiClient(API_KEY,addresses_form='user_friendly',testnet=True)
     #client = LsClient(ls_index=2, default_timeout=20)
     #await client.init_tonlib()
 
@@ -21,13 +25,17 @@ async def trans_to_wallet(end_adress, SUMMA):
 
     print(new_wallet.address) #вывод кошелёк
     print(new_wallet.mnemonics)
-    balance = await new_wallet.get_balance()
-    print(balance)
+
+    tonapi = Tonapi(api_key=API_KEY,is_testnet=True)
+    account = tonapi.accounts.get_info(account_id=ACCOUNT_ID)
+
+    balance = account.balance.to_amount()
+    print(f"Balance TON: {balance}")
 
     while(SUMMA > balance):
         time.sleep(5)
-        print(balance) #выводится баланс нового кошелька
-        balance = await new_wallet.get_balance()
+        print(f"Balance TON: {balance}") #выводится баланс нового кошелька
+        balance = account.balance.to_amount()
 
     print("успешный перевод на новый кошелёк")
 
